5c43e22afc50c0a3abaac6d9c5df57280359fbd6
Jéssica Rosália <jessicarosalia678@gmail.com>
2022-06-24 20:31:11 -0300

    Retirado console.log

--- src/services/index.js
+++ src/services/index.js
@@ -2,15 +2,12 @@ import { api } from "./api";
 
 export async function postDestino(destino){
     const response = await api.post('/formulario/destinos', destino, {
-        headers: {}
     });
     return response.data;
 }
 
 export async function login(user) {
-    console.log(user);
     const response = await api.post('/login', user, {
-        headers: {}
     });
     return response;
 }
